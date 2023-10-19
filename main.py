55a809f2b3e64c5ccdefe342af3154a3f7dc0283
dataMunkhnaran/Muna <data.munkhnaran@gmail.com>
2023-10-19 17:04:30 +0800

    newww

--- main.py
+++ main.py
@@ -27,3 +27,42 @@ BATCH_SIZE = 8
 MAX_EPOCHS = 20
 CHECKPOINT_DIR = f'/kaggle/working/'
 EXPERIMENT_NAME = f'yolo_nas_face'
+
+train_data = coco_detection_yolo_format_train(
+    dataset_params={
+        'data_dir': dataset_params['data_dir'],
+        'images_dir': dataset_params['train_images_dir'],
+        'labels_dir': dataset_params['train_labels_dir'],
+        'classes': dataset_params['classes']
+    },
+    dataloader_params={
+        'batch_size': BATCH_SIZE,
+        'num_workers': 1
+    }
+)
+
+val_data = coco_detection_yolo_format_val(
+    dataset_params={
+        'data_dir': dataset_params['data_dir'],
+        'images_dir': dataset_params['val_images_dir'],
+        'labels_dir': dataset_params['val_labels_dir'],
+        'classes': dataset_params['classes']
+    },
+    dataloader_params={
+        'batch_size': BATCH_SIZE,
+        'num_workers': 1
+    }
+)
+
+test_data = coco_detection_yolo_format_val(
+    dataset_params={
+        'data_dir': dataset_params['data_dir'],
+        'images_dir': dataset_params['test_images_dir'],
+        'labels_dir': dataset_params['test_labels_dir'],
+        'classes': dataset_params['classes']
+    },
+    dataloader_params={
+        'batch_size': BATCH_SIZE,
+        'num_workers': 1
+    }
+)
